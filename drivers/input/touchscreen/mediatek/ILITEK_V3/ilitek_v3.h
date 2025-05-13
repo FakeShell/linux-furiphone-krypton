b83ae87e27c3485a2a1530b0b8e71250ed3ceb1e
Bardia Moshiri <bardia@furilabs.com>
2025-05-12 20:14:01 -0400

    TEMP: ilitek: make everything pr_info
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- drivers/input/touchscreen/mediatek/ILITEK_V3/ilitek_v3.h
+++ drivers/input/touchscreen/mediatek/ILITEK_V3/ilitek_v3.h
@@ -174,19 +174,19 @@
 
 #define ILI_INFO(fmt, arg...)						\
 ({									\
-	pr_debug("ILITEK: (%s, %d): " fmt, __func__, __LINE__, ##arg);	\
+	pr_info("ILITEK: (%s, %d): " fmt, __func__, __LINE__, ##arg);	\
 })									\
 
 #define ILI_ERR(fmt, arg...)						\
 ({									\
-	pr_err("ILITEK: (%s, %d): " fmt, __func__, __LINE__, ##arg);	\
+	pr_info("ILITEK: (%s, %d): " fmt, __func__, __LINE__, ##arg);	\
 })									\
 
 extern bool debug_en;
 #define ILI_DBG(fmt, arg...)						\
 do {									\
 	if (1)						\
-	pr_debug("ILITEK: (%s, %d): " fmt, __func__, __LINE__, ##arg);	\
+	pr_info("ILITEK: (%s, %d): " fmt, __func__, __LINE__, ##arg);	\
 } while (0)
 
 #define ERR_ALLOC_MEM(X)	((IS_ERR(X) || X == NULL) ? 1 : 0)
