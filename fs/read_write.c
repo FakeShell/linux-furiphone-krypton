14954cc43031ee8c86f761acea11300b322b9d98
Jesús Higueras <jesus@furilabs.com>
2025-12-02 10:52:54 +0100

    Revert "fs: partially remove mandatory file locking stuff from rw_verify_area"
    This reverts commit b34d57734686302105638a24825d8b70421ba796.

--- fs/read_write.c
+++ fs/read_write.c
@@ -382,6 +382,12 @@ int rw_verify_area(int read_write, struct file *file, const loff_t *ppos, size_t
 			return retval;
 	}
 
+	if (unlikely(inode->i_flctx && mandatory_lock(inode))) {
+		retval = locks_mandatory_area(inode, file, pos, pos + count - 1,
+				read_write == READ ? F_RDLCK : F_WRLCK);
+		if (retval < 0)
+			return retval;
+	}
 	return security_file_permission(file,
 				read_write == READ ? MAY_READ : MAY_WRITE);
 }
